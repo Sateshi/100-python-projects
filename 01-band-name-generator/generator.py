c77e1924282d1c1a17da3cc4c97b1464d4ce40ca
Sateshi <sateshiplay@gmail.com>
2022-10-10 11:37:07 +0200

    project 1 band name generator done JB

---
+++ 01-band-name-generator/generator.py
@@ -0,0 +1,8 @@
+def main():
+    print("Welcome to The Band Name Generator!")
+    city = input("What city did you grow up in ?\n")
+    pet = input("What was your first pet ?\n")
+    print("Your band name is "+ city + " " + pet)
+    
+if __name__ == "__main__":
+    main()
